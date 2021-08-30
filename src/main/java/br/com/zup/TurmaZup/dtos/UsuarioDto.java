58b0450da73639062b0a2216d2df594e2d48f290
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-30 18:21:38 -0300

    Subindo classe usuario Dto

--- src/main/java/br/com/zup/TurmaZup/dtos/UsuarioDto.java
+++ src/main/java/br/com/zup/TurmaZup/dtos/UsuarioDto.java
@@ -1,2 +1,63 @@
-package br.com.zup.TurmaZup.dtos;public class UsuarioDto {
+package br.com.zup.TurmaZup.dtos;
+
+import br.com.zup.TurmaZup.models.UsuarioModel;
+
+
+    public class UsuarioDto {
+
+        private int id;
+        private String nome;
+        private String email;
+        private String cargo;
+
+        public UsuarioDto() {
+
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getCargo() {
+            return cargo;
+        }
+
+        public void setCargo(String cargo) {
+            this.cargo = cargo;
+        }
+
+        public static UsuarioDto converterModelParaDTO(UsuarioModel usuario){
+            UsuarioDto usuarioDto = new UsuarioDto();
+            usuarioDto.setNome(usuario.getNome());
+            usuarioDto.setEmail(usuario.getEmail());
+            usuarioDto.setCargo(usuario.getCargo());
+            usuarioDto.setId(usuario.getId());
+
+            return usuarioDto;
+
+
+
+
+
+    }
 }
