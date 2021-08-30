0a5461ba30cb312c8144cceffb9033d785da9eb3
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-30 10:33:35 -0300

    Subindo classe componentes

--- src/main/java/br/com/zup/TurmaZup/componentes/Conversor.java
+++ src/main/java/br/com/zup/TurmaZup/componentes/Conversor.java
@@ -1,2 +1,16 @@
-package br.com.zup.TurmaZup.componentes;public class Conversor {
+package br.com.zup.TurmaZup.componentes;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+
 }
