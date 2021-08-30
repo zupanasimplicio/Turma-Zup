e62ee242122e0359fca8aec16c3470c4dc9cb296
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-30 18:34:06 -0300

    subindo classe usuario Repository

--- src/main/java/br/com/zup/TurmaZup/repositories/UsuarioRepository.java
+++ src/main/java/br/com/zup/TurmaZup/repositories/UsuarioRepository.java
@@ -3,5 +3,9 @@ package br.com.zup.TurmaZup.repositories;
 import br.com.zup.TurmaZup.models.UsuarioModel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer> {
+
+    Optional<UsuarioModel> findByEmail(String email);
 }
