e8a2517de7c232350b03a5392b3865e7a24bed1e
Ana Paula Simplicio <ana.simplicio@zup.com.br>
2021-08-30 10:36:42 -0300

    Subindo classe Chatcontroller

--- src/main/java/br/com/zup/TurmaZup/controllers/ChatController.java
+++ src/main/java/br/com/zup/TurmaZup/controllers/ChatController.java
@@ -1,4 +1,42 @@
 package br.com.zup.TurmaZup.controllers;
 
+import br.com.zup.TurmaZup.dtos.ChatDto;
+import br.com.zup.TurmaZup.dtos.MensagemDto;
+import br.com.zup.TurmaZup.dtos.MensagemEmResumoDto;
+import br.com.zup.TurmaZup.models.MensagemModel;
+import br.com.zup.TurmaZup.services.MensagemService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("chat")
 public class ChatController {
+
+    @Autowired
+    MensagemService mensagemService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping()
+    public MensagemDto cadastrarMensagem(@RequestBody @Valid ChatDto chatDto) {
+        MensagemModel mensagem = chatDto.converterDTOemMensagem();
+        mensagem = mensagemService.cadastrarMensagem(chatDto.getOrigem(), chatDto.getDestino(), mensagem);
+        return MensagemDto.converterModelParaDTO(mensagem);
+    }
+
+    @GetMapping("/usuario/perfil/{emailUsuario}")
+    public List<MensagemEmResumoDto.MensagemResumoDto> pesquisarMensagens(@PathVariable String emailUsuario,
+                                                                          @RequestParam(required = false) Boolean visualizado){
+        List<MensagemModel> mensagens = mensagemService.filtrarMengensPor(emailUsuario, visualizado);
+        List<MensagemEmResumoDto.MensagemResumoDto> mensagemResumoDtos = mensagens.stream()
+                .map(mensagem -> modelMapper.map(mensagem, MensagemEmResumoDto.MensagemResumoDto.class)).collect(Collectors.toList());
+        return mensagemResumoDtos;
+    }
+
 }
